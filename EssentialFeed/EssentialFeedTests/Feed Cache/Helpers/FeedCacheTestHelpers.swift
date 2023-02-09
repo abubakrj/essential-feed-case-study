c5ad6fdb64ebb0b3d9460b9542b938985b341480
abubakrr <abubakr.jabbarov@gmail.com>
2023-02-09 10:33:27 +0500

    upd date helper

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -1,3 +1,7 @@
+//
+//  Copyright © 2019 Essential Developer. All rights reserved.
+//
+
 import Foundation
 import EssentialFeed
 
@@ -13,13 +17,19 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
 
 extension Date {
     func minusFeedCacheMaxAge() -> Date {
-        return adding(days: -7)
-    }
-    
-    func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+        return adding(days: -feedCacheMaxAgeInDays)
     }
 
+    private var feedCacheMaxAgeInDays: Int {
+        return 7
+    }
+
+    private func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+}
+
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
     }
